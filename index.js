2d330b48ca09eab0782f191cc774f76d8d169e58
Aruneet <87275550+Aruneet-11@users.noreply.github.com>
2023-06-10 07:31:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+    //detecting the button pressed.
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var buttonInnerHTML=this.innerHTML;
+        pressKey(buttonInnerHTML);
+        addAnimation(buttonInnerHTML);
+});
+}
+// detcting the keyboard pressed.
+document.addEventListener('keydown',function(event)// event will detect the key which has been pressed.
+{
+   pressKey(event.key);
+   addAnimation(event.key);
+});
+function pressKey(key)
+   {
+    switch(key)
+    {
+       case('w'):
+    
+        var audio1=new Audio("sounds/tom-4.mp3");
+        audio1.play();
+        break;
+    case('a'):
+    
+        var audio2=new Audio("sounds/tom-2.mp3");
+        audio2.play();
+        break;
+    case('s'):
+    
+        var audio3=new Audio("sounds/tom-3.mp3");
+        audio3.play();
+         break;
+    case('d'):
+    
+        var audio4=new Audio("sounds/tom-1.mp3");
+        audio4.play();
+        break;
+    case('j'):
+    
+        var audio5=new Audio("sounds/snare.mp3");
+        audio5.play();
+         break;
+    case('k'):
+    
+        var audio6=new Audio("sounds/crash.mp3");
+        audio6.play();
+        break;
+    case('l'):
+      var audio7=new Audio("sounds/kick-bass.mp3");
+      audio7.play();
+      break;
+
+    default:
+        console.log(buttonInnerHTML);
+    }
+   }
+   function addAnimation(currentKey)
+   {
+    var currentButton=document.querySelector("."+currentKey);
+         currentButton.classList.add("pressed");
+
+     setTimeout(function()
+     {
+        currentButton.classList.remove("pressed");
+     },100);
+   }
